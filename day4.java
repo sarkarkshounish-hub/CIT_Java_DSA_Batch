cafc94212ca17a5374f26bf7a723503f48bd72e0
sarkarkshounish-hub <sarkarkshounish@gmail.com>
2026-08-03 16:18:50 +0530

    Create day4.java

---
+++ day4.java
@@ -0,0 +1,112 @@
+import java.util.Scanner;
+public class day4 {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        // String name = "Kshounish";
+        // int age = 26;
+        // double height = 5.8;
+        // char grade = 'A';
+        // boolean isStudent = true;
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("Height: " + height);
+        // System.out.println("Grade: " + grade);
+        // System.out.println("Is Student: " + isStudent);
+
+        // double length = 10;
+        // double width = 5;
+        // double area = length * width;
+        // System.out.println("Length: " + length);
+        // System.out.println("Width: " + width);
+        // System.out.println("Area of Rectangle: " + area);
+
+        // System.out.print("Enter first number: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter second number: ");
+        // int b = sc.nextInt();
+        // System.out.println("Before Swapping:");
+        // System.out.println("a = " + a);
+        // System.out.println("b = " + b);
+        // a = a + b;
+        // b = a - b;
+        // a = a - b;
+        // System.out.println("After Swapping:");
+        // System.out.println("a = " + a);
+        // System.out.println("b = " + b);
+
+        // int a = 15;
+        // int b = 4;
+        // System.out.println("Addition = " + (a + b));
+        // System.out.println("Subtraction = " + (a - b));
+        // System.out.println("Multiplication = " + (a * b));
+        // System.out.println("Division = " + (a / b));
+        // System.out.println("Modulus = " + (a % b));
+
+        // System.out.print("Enter first number: ");
+        // int a = sc.nextInt();
+        // System.out.print("Enter second number: ");
+        // int b = sc.nextInt();
+        // int largest = (a > b) ? a : b;
+        // System.out.println("Largest number = " + largest);
+
+        // String str1 = "Hello";
+        // String str2 = new String("Hello");
+        // String str3 = "hello";
+        // System.out.println("Using equals():");
+        // System.out.println(str1.equals(str2));
+        // System.out.println("Using == :");
+        // System.out.println(str1 == str2);
+        // System.out.println("Using equalsIgnoreCase():");
+        // System.out.println(str1.equalsIgnoreCase(str3));
+
+        // System.out.print("Enter Name: ");
+        // String name = sc.nextLine();
+        // System.out.print("Enter Age: ");
+        // int age = sc.nextInt();
+        // System.out.print("Enter CGPA: ");
+        // double cgpa = sc.nextDouble();
+        // System.out.println("\nStudent Information");
+        // System.out.println("Name: " + name);
+        // System.out.println("Age: " + age);
+        // System.out.println("CGPA: " + cgpa);
+
+        // double balance = 5000;
+        // System.out.println("ATM Menu");
+        // System.out.println("1. Balance");
+        // System.out.println("2. Deposit");
+        // System.out.println("3. Withdraw");
+        // System.out.println("4. Exit");
+        // System.out.print("Enter your choice: ");
+        // int choice = sc.nextInt();
+        // switch (choice) {
+        //     case 1:
+        //         System.out.println("Balance: " + balance);
+        //         break;
+        //     case 2:
+        //         System.out.print("Enter deposit amount: ");
+        //         double deposit = sc.nextDouble();
+        //         balance = balance + deposit;
+        //         System.out.println("New Balance: " + balance);
+        //         break;
+        //     case 3:
+        //         System.out.print("Enter withdrawal amount: ");
+        //         double withdraw = sc.nextDouble();
+        //         if (withdraw <= balance) {
+        //             balance = balance - withdraw;
+        //             System.out.println("New Balance: " + balance);
+        //         } else {
+        //             System.out.println("Insufficient Balance");
+        //         }
+        //         break;
+        //     case 4:
+        //         System.out.println("Thank You!");
+        //         break;
+        //     default:
+        //         System.out.println("Invalid Choice");
+        // }
+
+        
+
+        sc.close();
+    }
+}
